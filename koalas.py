7621ec5aef8fb7c60c03476eabd174d4842f1b71
eotles <eotles@gmail.com>
2016-03-10 09:41:16 -0600

    updates to dataframe and testing

--- koalas.py
+++ koalas.py
@@ -9,16 +9,22 @@ class DataFrame(object):
             index = [i for i in xrange(len(data))]
         if(columns is None):
             columns = []
+        
+        data_at_idx = {idx: [] for idx in index}
         data_in_col = {column: [] for column in columns}
         
         rows_are_lists = False
         rows_are_dicts = False
         for index_pointer, row in enumerate(data):
+            print(row)
+            if(isinstance(row, (list, dict)) == False):
+                raise ValueError('Data must be two dimensional')
             if(rows_are_lists and rows_are_dicts): 
                 raise ValueError('Data contains both lists and dicts')
             
             if(type(row) is list):
                 rows_are_lists = True
+                data_at_idx[index_pointer] = row
                 if(len(columns) < len(row)):
                     raise ValueError('Rows are longer than columns specify')
                 for col_pointer, element in enumerate(row):
@@ -28,6 +34,7 @@ class DataFrame(object):
                 rows_are_dicts = True
                 
         self.data = data
+        self.data_at_idx = data_at_idx
         self.data_in_col = data_in_col    
         self.index = index
         self.columns = columns
--- test.py
+++ test.py
@@ -1,9 +1,26 @@
 import koalas as kl
 
 def main():
+    
     #no args
     kl.DataFrame()
 
+    #one row of data, no other args
+    data = [[1,2]]
+    kl.DataFrame(data)
+
+    #one row of data, index, no columns
+    index = ['a']
+    kl.DataFrame(data, index)
+
+    #one row of data, index, columns
+    index = ['a']
+    columns = ['z', 'y']
+    kl.DataFrame(data, index, columns)
+
+    index = ['a']
+    columns = ['z']
+    kl.DataFrame(data, index, columns)
 
 
 
